ba421b7cec76d3c309a43ff86bc4698f8bcf4ec9
hsoubaacademy <hsoubaacademy@gmail.com>
2021-06-10 22:34:58 +0300

    master

---
+++ app/Database/DBConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database;
+
+class DBConnection 
+{
+    private static $pdo;
+    public static function make($config)
+    {
+        $dsn = $config['dsn'] ?? 
+        "{$config['connection']}:host={$config['host']};dbname={$config['name']}";
+        try {
+            self::$pdo = self::$pdo ? 
+                :new \PDO($dsn, $config['user'], $config['password']);
+                return self::$pdo;
+            } catch (\PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+}
---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return [
+    'app' => [
+        'home_url' => $_ENV['APP_URL']
+    ],
+    'database' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'connection' => $_ENV['DB_CONNECTION'] ?? 'mysql',
+        'host' => $_ENV['DB_HOST'],
+        'name' => $_ENV['DB_NAME'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
